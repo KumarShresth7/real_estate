76d95e5b0bcd1fb060091bb3181139f2bdd9446c
KumarShresth7 <kumarshresth2004@gmail.com>
2025-01-03 23:06:26 +0530

    completed all setup

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import { Link, useLocalSearchParams } from "expo-router";
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10 font-rubik">Welcome to Real Estate App</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/[id]">Property</Link>
+    </View>
+  );
+}
